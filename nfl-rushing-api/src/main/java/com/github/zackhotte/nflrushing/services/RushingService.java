7d9ff84341e610c3010cf968fec298708553dd18
Zachary Hotte <zachary.hotte@workjam.com>
2022-01-31 22:17:46 -0500

    Modified comparator based on sort direction

--- nfl-rushing-api/src/main/java/com/github/zackhotte/nflrushing/services/RushingService.java
+++ nfl-rushing-api/src/main/java/com/github/zackhotte/nflrushing/services/RushingService.java
@@ -1,6 +1,7 @@
 package com.github.zackhotte.nflrushing.services;
 
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.github.zackhotte.nflrushing.controllers.requests.RequestParam;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -44,31 +47,33 @@ public class RushingService {
     }
 
     private Page<Rusher> process(Page<Rusher> rushers, Pageable pageable) {
-        if (hasSortingField(pageable, CUSTOM_SORTING_FIELD)) {
+        var order = getSortingField(pageable, CUSTOM_SORTING_FIELD);
+        if (order.isPresent()) {
+            var direction = order.get().getDirection();
             return new PageImpl<>(
-                    rushers.getContent().stream().sorted(longestRunCustomSorter()).collect(Collectors.toList()),
+                    rushers.getContent().stream().sorted(longestRunCustomSorter(direction))
+                            .collect(Collectors.toList()),
                     pageable,
-                    rushers.getTotalElements()
-            );
+                    rushers.getTotalElements());
         }
 
         return rushers;
     }
 
-    private boolean hasSortingField(Pageable pageable, String fieldName) {
+    private Optional<Order> getSortingField(Pageable pageable, String fieldName) {
         var sort = pageable.getSort();
         for (var order : sort) {
             var prop = order.getProperty();
 
             if (prop.equalsIgnoreCase(fieldName)) {
-                return true;
+                return Optional.of(order);
             }
         }
 
-        return false;
+        return Optional.empty();
     }
 
-    private Comparator<Rusher> longestRunCustomSorter() {
+    private Comparator<Rusher> longestRunCustomSorter(Direction direction) {
         return (r1, r2) -> {
             var lng1 = r1.getLongestRun();
             var lng2 = r2.getLongestRun();
@@ -79,9 +84,10 @@ public class RushingService {
             var val1 = Integer.parseInt(lng1);
             var val2 = Integer.parseInt(lng2);
 
-            return Integer.compare(val1, val2);
+            return direction == Direction.ASC
+                    ? Integer.compare(val1, val2)
+                    : Integer.compare(val2, val1);
         };
     }
-
 
 }
